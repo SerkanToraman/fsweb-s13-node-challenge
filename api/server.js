562a0dcfb3f3c3f3902ce041ee04423a81ff5147
serkantoraman <serkan@github.com>
2023-06-16 09:07:22 +0300

    npm installations done and server is up and running

--- api/server.js
+++ api/server.js
@@ -1,9 +1,30 @@
-const express = require('express');
-const server = express();
 
 // Sunucunuzu yapılandırın
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
 // Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
 // Bu dosyanın içinde `server.listen()` YAPMAYIN!
+
+//1 Imports
+const express = require('express');
+const server = express();
+
+
+//2 Middlewares 
+server.use(express.json());
+
+//3 Routers
+// //Check if server is running
+// server.get('/',(req,res)=>{
+//   res.json("server deneme - basarili")
+// })
+
+
+
+
+//4 Errors 
+
+
+
+//5 Exports
 
 module.exports = server;
